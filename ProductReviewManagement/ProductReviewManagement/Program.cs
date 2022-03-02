7f253459b911fa90eef4b7a8d95173fbfc264bb5
akshata2005 <akshatasawant1805@gmail.com>
2022-03-02 19:27:48 +0530

    [Akshata] Add:Find Record based on product id

--- ProductReviewManagement/ProductReviewManagement/Program.cs
+++ ProductReviewManagement/ProductReviewManagement/Program.cs
@@ -41,6 +41,8 @@ namespace ProductReviewManagement
             }
             Console.WriteLine("Top three records");
             ProductManagement.TopRecords(productReviewList);
+            Console.WriteLine("Record based on ProductID");
+            ProductManagement.SelectedRecord(productReviewList);
         }
     }
 }
